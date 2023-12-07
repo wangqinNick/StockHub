85f16360ebcada5f7aaf9d4b73fbf4b23b1a80d1
Sparks2001 <50083580+wangqinNick@users.noreply.github.com>
2023-12-07 20:03:08 +0800

    feat(stock job): Inherited xxljob for systematic periodic collection of stock market data
    This commit includes the following enhancements:
    - Integrated xxljob to facilitate the scheduled and systematic retrieval of stock market data
    - Enabled automated periodic collection of stock data to ensure up-to-date information is consistently available
    - Streamlined the data collection process for improved efficiency and reliability

---
+++ stock_job/src/main/java/com/wangqin/stock/job/handler/StockJobHandler.java
@@ -0,0 +1,47 @@
+package com.wangqin.stock.job.handler;
+
+import com.wangqin.stock.service.StockTimerTaskService;
+import com.xxl.job.core.handler.annotation.XxlJob;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * 定义xxljob的任务执行器的Bean
+ */
+@Component
+public class StockJobHandler {
+
+    /**
+     * 注入股票定时任务服务bean
+     */
+    @Autowired
+    private StockTimerTaskService stockTimerTaskService;
+
+    /**
+     * 定义定时任务，采集国内大盘数据
+     */
+    @XxlJob("getStockInnerMarketInfos")
+    public void getStockInnerMarketInfos(){
+
+        stockTimerTaskService.getInnerMarketInfo();
+        System.out.println("定时任务，采集国内大盘数据执行");
+    }
+
+    /**
+     * 定时采集A股个股数据
+     */
+    @XxlJob("getStockInfos")
+    public void getStockInfos(){
+        stockTimerTaskService.getStockRtIndex();
+        System.out.println("定时任务，采集A股个股数据执行");
+    }
+
+    /**
+     * 定时采集A股板块数据
+     */
+    @XxlJob("getBlockInfos")
+    public void getBlockInfos(){
+        stockTimerTaskService.getBlockIndex();
+        System.out.println("定时任务，采集A股板块数据执行");
+    }
+}
--- stock_job/src/main/java/com/wangqin/stock/service/impl/StockTimerTaskServiceImpl.java
+++ stock_job/src/main/java/com/wangqin/stock/service/impl/StockTimerTaskServiceImpl.java
@@ -112,9 +112,7 @@ public class StockTimerTaskServiceImpl implements StockTimerTaskService {
             // 大盘数据采集、插入完毕后, 通知backend模块刷新缓存
             // 数据库 -> 缓存
             rabbitTemplate.convertAndSend("stockExchange","inner.market",new Date());
-
         }
-
 
         else log.error("当前时间: {}, 插入大盘数据失败", DateTime.now().toString("yyyy-MM-dd HH:mm:ss"));
     }
