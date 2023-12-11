9e112ff06d3b5b4f316875f8c0373d1ab8b08f6d
Sparks2001 <50083580+wangqinNick@users.noreply.github.com>
2023-12-11 11:16:50 +0800

    feat: Implement uniform Long-to-String serialization configuration in configuration class
    This commit introduces a streamlined approach to Long-to-String serialization by centralizing the configuration within the designated configuration class. By unifying the serialization strategy for Long values to String format, we enhance consistency and maintainability across the codebase. This ensures a standardized implementation of Long serialization, promoting clarity and ease of maintenance.

--- stock_backend/src/main/java/com/wangqin/stock/config/CommonConfig.java
+++ stock_backend/src/main/java/com/wangqin/stock/config/CommonConfig.java
@@ -1,12 +1,15 @@
 package com.wangqin.stock.config;
 
-import com.github.benmanes.caffeine.cache.Cache;
-import com.github.benmanes.caffeine.cache.Caffeine;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import com.wangqin.stock.pojo.vo.StockInfoConfig;
 import com.wangqin.stock.utils.IdWorker;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -32,6 +35,27 @@ public class CommonConfig {
     @Bean
     public IdWorker idWorker() {
         return new IdWorker(1L, 1L);    // no.1 machine, no.1 datacenter
+    }
+
+    /**
+     * 统一定义Long序列化转String设置
+     *
+     * @return MappingJackson2HttpMessageConverter
+     */
+    @Bean
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        ObjectMapper objectMapper = new ObjectMapper();
+        SimpleModule simpleModule = new SimpleModule();
+
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
+        simpleModule.addSerializer(Long.TYPE, ToStringSerializer.instance);
+
+        objectMapper.registerModule(simpleModule);
+        converter.setObjectMapper(objectMapper);
+
+        return converter;
     }
 }
 
